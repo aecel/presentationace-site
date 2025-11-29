90936312d8d45cde023e7e6b0d4c80fd509dbff2
aecel <aecelacuna@gmail.com>
2025-11-29 13:43:48 +0800

    Add Github Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,8 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config"
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: "https://presentationace.com", // your custom domain (future canonical URL)
+  base: "/", // because the custom domain will point to the root of the project
+})
