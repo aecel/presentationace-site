8ec93b331cd5208cb7824f27c7b13ebfb9b6d998
aecel <aecelacuna@gmail.com>
2026-01-18 19:55:51 +0800

    Add canonical to head of BaseLayout, add sitemap, robots, and llms

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
-import { defineConfig } from "astro/config"
 
+import { defineConfig } from "astro/config"
+import sitemap from "@astrojs/sitemap"
 // https://astro.build/config
 export default defineConfig({
   site: "https://presentationace.com", // your custom domain (future canonical URL)
   base: "/", // because the custom domain will point to the root of the project
+  integrations: [sitemap()],
 })
