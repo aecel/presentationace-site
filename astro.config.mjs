ba143faf65b31f3f013f82aa55cac91970d9660a
aecel <aecelacuna@gmail.com>
2026-06-23 21:59:54 +0800

    Change onboarding routing stuff

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,19 @@
 import { defineConfig } from "astro/config"
 import sitemap from "@astrojs/sitemap"
 // https://astro.build/config
+
+const SECRET_PATHS = ["/welcome", "/start"]
+
 export default defineConfig({
   site: "https://presentationace.com", // your custom domain (future canonical URL)
   base: "/", // because the custom domain will point to the root of the project
   integrations: [
     sitemap({
-      // exclude any URL that contains "/onboarding"
-      filter: (page) => !page.includes("/onboarding"),
+      filter: (page) => {
+        // page is a full URL like "https://something.com/secret"
+        const url = new URL(page)
+        return !SECRET_PATHS.includes(url.pathname)
+      },
     }),
   ],
 })
